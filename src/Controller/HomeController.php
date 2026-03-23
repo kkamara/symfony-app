e8f0fc381fa480274ccfe4c8ee6e81a7dded4c1a
kkamara <kamaracomputers@gmail.com>
2026-03-23 08:29:53 +0000

    Render Twig template in a shorter way

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,7 +9,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class HomeController extends AbstractController {
   #[Route("/")]
   public function index(): Response {
-    $contents = $this->renderView("home/index.html.twig");
-    return new Response($contents);
+    return $this->render("home/index.html.twig");
   }
 }
