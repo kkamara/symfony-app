6aeca1c6bae24fe12e2f56fffcbcab8d761f0ce8
kkamara <kamaracomputers@gmail.com>
2026-03-23 09:43:42 +0000

    Show all products on product screen

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +10,10 @@ use Symfony\Component\Routing\Attribute\Route;
 final class ProductController extends AbstractController
 {
     #[Route('/products', name: 'product_index')]
-    public function index(): Response
+    public function index(ProductRepository $repo): Response
     {
         return $this->render('product/index.html.twig', [
-            'controller_name' => 'ProductController',
+            'products' => $repo->findAll(),
         ]);
     }
 }
