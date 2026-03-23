500b98249300ce9ed3adc00a40a73957318ff01d
kkamara <kamaracomputers@gmail.com>
2026-03-23 09:23:16 +0000

    Create seed data

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,13 +4,25 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use App\Entity\Product;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $product = new Product();
+        $product->setName('Product One');
+        $product->setDescription('This is a sample product.');
+        $product->setSize(100);
+
+        $manager->persist($product);
+
+        $product = new Product();
+        $product->setName('Product Two');
+        $product->setDescription('This is another sample product.');
+        $product->setSize(200);
+
+        $manager->persist($product);
 
         $manager->flush();
     }
